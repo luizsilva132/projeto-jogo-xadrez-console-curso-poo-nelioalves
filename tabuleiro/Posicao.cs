7eb4a6e165cc4a7afe049b2670a72cc156c105b7
Luiz Henrique Rosa Da Silva <luizsilva132132@outlook.com>
2024-01-09 11:11:44 -0300

    Criando a classe Posicao

---
+++ tabuleiro/Posicao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace tabuleiro
+{
+    internal class Posicao
+    {
+
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao()
+        {
+        }
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return linha
+                   + ", "
+                   + coluna;
+        }
+    }
+}
